b0b25dd7b56a0e59ac7f6b3f8c1552242e8cf579
burakhatipogluu <77885766+burakhatipogluu@users.noreply.github.com>
2021-03-21 23:26:25 +0300

    Add project files.

---
+++ Business/Abstract/ICarService.cs
@@ -0,0 +1,23 @@
+﻿using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface ICarService 
+    {
+
+        IResult Add(Car car);
+        IResult Delete(Car car);
+        IResult Update(Car car);
+        IDataResult<List<Car>> GetAll();
+        IDataResult<List<Car>> GetCarsByBrandId(int brandId);
+        IDataResult<List<Car>> GetCarsByColorId(int colorId);
+        IDataResult<List<CarDetailDto>> GetCarDetails();
+        
+    }
+}
